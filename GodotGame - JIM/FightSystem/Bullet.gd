ee4a2a5dd1b52fddbf2a13a97582603fd93b6fe8
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2022-03-17 17:25:28 +0200

    Merge branch 'anthony' into develop

--- GodotGame - JIM/FightSystem/Bullet.gd
+++ GodotGame - JIM/FightSystem/Bullet.gd
@@ -5,15 +5,14 @@ var hittable = false
 
 func _ready():
 	#warning-ignore:return_value_discarded
-	connect("body_entered", self, "_on_character_body_entered")
-	#look_at(get_global_mouse_position())
+	connect("body_entered", self, "_on_Bullet_body_entered")
 
 func _process(delta):
 	time += delta
-	#print("Time: %d" % time)
+	#print("Time: %d" % time) # debug
 	if time >= 2:
 		queue_free()
 	
-func _on_character_body_entered(body):
+func _on_Bullet_body_entered(body):
 	if body.hittable == true:
 		queue_free()
--- GodotGame - JIM/Player/Player.gd
+++ GodotGame - JIM/Player/Player.gd
@@ -5,11 +5,16 @@ const ACCELERATION = 900
 const MAX_SPEED = 250
 const FRICTION = 1500
 const BULLET_SPEED = 1000
+const BULLET_SPAWN_DISTANCE = 45
 
 var bullet = preload("res://FightSystem/Bullet.tscn")
-var bullet_amount = 100
+var bullet_amount = 100 # have to reload MAKE THIS!!
+var fire_rate = 0.3 # items change fire_rate or spread MAKE THIS!!
+var can_fire = true
+
 var velocity = Vector2.ZERO
-var hittable = false
+
+var hittable = true
 
 func _physics_process(delta): # if something changes over time, multiply with delta
 	var input_vector = Vector2.ZERO
@@ -30,14 +35,27 @@ func _physics_process(delta): # if something changes over time, multiply with de
 	velocity = move_and_slide(velocity) # velocity relative to delta automatically
 	
 	# shooting mechanic
-	if Input.is_action_just_pressed("LMB") and (bullet_amount >= 1):
+	if Input.is_action_just_pressed("LMB") and (bullet_amount >= 1) and can_fire:
 		fire_bullet()
 		if bullet_amount > 0:
 			bullet_amount -= 1
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
 
 # creating a bullet when fired
 func fire_bullet():
 	var bullet_instance = bullet.instance()
-	bullet_instance.position = get_global_position()
-	bullet_instance.apply_impulse(Vector2(), Vector2(BULLET_SPEED, 0).rotated(rotation))
-	get_tree().get_root().call_deferred("add_child", bullet_instance)
+	
+	var distance = get_global_mouse_position() - get_node("Position2D").get_global_position()
+	distance = distance.normalized()
+	print("Distance vector: {0}".format({"0":distance})) # debug
+	
+	#print("Rotation of Player: {0}".format({"0":rotation})) # debug
+	bullet_instance.position = get_node("Position2D").get_global_position() + distance * BULLET_SPAWN_DISTANCE
+	
+	bullet_instance.rotation = distance.angle()
+	bullet_instance.apply_impulse(Vector2(), Vector2(BULLET_SPEED, 0).rotated(bullet_instance.rotation))
+	
+	#get_tree().get_root().call_deferred("add_child", bullet_instance) # other way of adding child
+	get_tree().get_root().add_child(bullet_instance)
